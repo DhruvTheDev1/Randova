589661b4ed9588901c19a20078072a69eddefbbc
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-06-24 15:06:28 +0100

    Update: Updated GenerateDataController to display more data and main.css

--- RandomDataGenerator/src/main/java/com/example/RandomDataGenerator/controller/GenerateDataController.java
+++ RandomDataGenerator/src/main/java/com/example/RandomDataGenerator/controller/GenerateDataController.java
@@ -19,7 +19,7 @@ public class GenerateDataController {
   @GetMapping("/generated-random-data")
   public String generateRandomData(Model model) {
     List<RandomData> randomData = new ArrayList<>();
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 10; i++) {
       randomData.add(generateDataService.generateData());
     }
 
